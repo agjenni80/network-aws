93b069efecd8f2c70bcd45d33ba340be7b255a36
Jon Benson <bensojon@gmail.com>
2017-11-25 02:48:26 -0600

    Add image_id example to simple

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,6 +1,27 @@
+data "aws_ami" "base" {
+  most_recent = true
+  owners      = ["${var.ami_owner}"]
+
+  filter {
+    name   = "name"
+    values = ["${var.ami_name}"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 module "network_aws" {
   source = "../../../network-aws"
   # source = "git@github.com:hashicorp-modules/network-aws.git?ref=f-refactor"
 
-  name = "${var.name}"
+  name     = "${var.name}"
+  image_id = "${data.aws_ami.base.id}"
 }
